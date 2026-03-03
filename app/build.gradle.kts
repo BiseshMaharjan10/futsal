ef96a68d4486d1444658d8a71665b06f3d766d7e
BiseshMaharjan10 <biseshmhrz01@gmail.com>
2026-03-03 13:32:47 +0545

    fixed error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.cloudinary.android)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
