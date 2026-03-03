b26a537295413edc9f9f7db09b49b7debe22938d
BiseshMaharjan10 <biseshmhrz01@gmail.com>
2026-03-03 13:33:38 +0545

    made ui better

--- app/src/main/java/com/example/futsal/model/SubjectModel.kt
+++ app/src/main/java/com/example/futsal/model/SubjectModel.kt
@@ -4,14 +4,16 @@ data class SubjectModel(
     val id: String = "",
     val subjectName: String = "",
     val teacherName: String = "",
-    val creditHours: String = ""
+    val creditHours: String = "",
+    val bookImageUrl: String = ""
 ) {
     fun toMap(): Map<String, Any?> {
         return mapOf(
             "id" to id,
             "subjectName" to subjectName,
             "teacherName" to teacherName,
-            "creditHours" to creditHours
+            "creditHours" to creditHours,
+            "bookImageUrl" to bookImageUrl
         )
     }
 }
